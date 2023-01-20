fa2e6f6ea418043d66ad856f1b9c2f9d878e2a42
jkenzer <jkenzer@gmail.com>
2023-01-20 07:07:57 -0700

    Adding fraction library and displaying basic calculation

--- src/modules/calculator/app/app.js
+++ src/modules/calculator/app/app.js
@@ -1,3 +1,11 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
+import fc from "fraction-calculator";
 
-export default class HelloWorldApp extends LightningElement {}
+export default class WoodCalc extends LightningElement {
+  currentResult = 0;
+  // https://github.com/dennis-jiang/fraction-calculator#readme
+  get currentValue() {
+    this.currentResult = fc("1/2").minus("1/4").toFraction();
+    return this.currentResult;
+  }
+}
